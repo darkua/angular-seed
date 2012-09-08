4fd44b886181eb3ea02d7f75d5fc0a8ce854e6ee
Yuriy Silvestrov <yuriy@silvestrov.com>
2012-09-08 13:24:29 +0300

    Added twitter bootstrap

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,3 +11,15 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+function NavCntrl($scope, $location) {
+  $scope.tabs = [
+    {name:'View 1', path:"view1"},
+    {name:'View 2', path:"view2"}
+  ];
+  $scope.isActive = function(page)
+  {
+    return (new RegExp(page).test($location.path()))?"active":"";
+  };
+}
+MyCtrl2.$inject = ['$scope', '$location'];
